a80677637017ced578f14de533bea3da89c2f226
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2023-12-24 20:48:48 +0200

    Update mira.js

--- js/mira.js
+++ js/mira.js
@@ -1,68 +1,146 @@
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-      /*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+const themeButton = document.getElementById('theme-button');
+/*=============== Search ===============*/
+const searchButton = document.getElementById('search-button'),
+      searchClose = document.getElementById('search-close'),
+      searchContent = document.getElementById('search-content')
 
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', () =>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
-    }
+/*===== Search SHOW =====*/
+/* Validate if constant exists */
+if(searchButton){
+    searchButton.addEventListener('click', () =>{
+        searchContent.classList.add('show-search')
+    })
 }
 
-showMenu('nav-toggle','nav-menu')
-
-/*==================== SWIPER JS ====================*/
-let galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 0,
-    slidesPerView: 0,
-})
-
-let galleryTop = new Swiper('.gallery-top', {
-    effect: 'fade',
-    loop: true,
-
-    thumbs: {
-      swiper: galleryThumbs
-    }
-})
+/*===== Search HIDDEN =====*/
+/* Validate if constant exists */
+if(searchClose){
+    searchClose.addEventListener('click', () =>{
+        searchContent.classList.remove('show-search')
+    })
+}
+/*=============== SEARCH ===============*/
 
 
-/*==================== POPUP ====================*/
-// const btnOpenVideo = document.querySelectorAll('.islands__video-content')
-// const islandsPopup = document.getElementById('popup')
+/*=============== LOGIN ===============*/
 
-// function poPup(){
-//     islandsPopup.classList.add('show-popup')
-// }
-// btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
 
-// const btnCloseVideo = document.getElementById('popup-close')
+// JavaScript time
+var current_fs, next_fs, previous_fs; // fieldsets
+var left, opacity, scale; // fieldset properties which we will animate
+var animating; // flag to prevent quick multi-click glitches
 
-// btnCloseVideo.addEventListener('click', ()=> {
-//     islandsPopup.classList.remove('show-popup')
-// })
+document.querySelectorAll(".next").forEach(function(element) {
+  element.addEventListener("click", function() {
+    if (animating) return false;
+    animating = true;
 
-/*==================== GSAP ANIMATION ====================*/
-const controlImg = document.querySelectorAll('.controls__img')
+    current_fs = this.parentElement;
+    next_fs = this.parentElement.nextElementSibling;
 
-// function scrollAnimation(){
-//     gsap.from('.islands__subtitle', {opacity: 0, duration: .2, delay: .2, y: -20})
-//     gsap.from('.islands__title', {opacity: 0, duration: .3, delay: .3, y: -20})
-//     gsap.from('.islands__description', {opacity: 0, duration: .4, delay: .4, y: -20})
-//     gsap.from('.islands__button', {opacity: 0, duration: .5, delay: .5, y: -20})
-//     gsap.from('.islands__video-content', {opacity: 0, duration: .6, delay: .6, y: -20})
+    // activate next step on progressbar using the index of next_fs
+    document.querySelectorAll("fieldset").forEach(function(fieldset, index) {
+      if (fieldset === next_fs) {
+        document.querySelectorAll("#progressbar li")[index].classList.add("active");
+      }
+    });
 
-//     islandsPopup.classList.remove('show-popup')
-// }
+    // show the next fieldset
+    next_fs.style.display = "block";
 
-// controlImg.forEach(c => c.addEventListener('click', scrollAnimation))
+    // hide the current fieldset with style
+    var animationInterval = setInterval(function() {
+      current_fs.style.opacity -= 0.01;
 
+      // as the opacity of current_fs reduces to 0 - stored in "now"
+      // 1. scale current_fs down to 80%
+      scale = 1 - (1 - current_fs.style.opacity) * 0.2;
+      // 2. bring next_fs from the right(50%)
+      left = (current_fs.style.opacity * 50) + "%";
+      // 3. increase opacity of next_fs to 1 as it moves in
+      opacity = 1 - current_fs.style.opacity;
+      current_fs.style.transform = 'scale(' + scale + ')';
+      next_fs.style.left = left;
+      next_fs.style.opacity = opacity;
+
+      if (current_fs.style.opacity <= 0) {
+        current_fs.style.display = 'none';
+        animating = false;
+        clearInterval(animationInterval);
+      }
+    }, 5);
+  });
+});
+
+document.querySelectorAll(".previous").forEach(function(element) {
+  element.addEventListener("click", function() {
+    if (animating) return false;
+    animating = true;
+
+    current_fs = this.parentElement;
+    previous_fs = this.parentElement.previousElementSibling;
+
+    // de-activate current step on progressbar
+    document.querySelectorAll("fieldset").forEach(function(fieldset, index) {
+      if (fieldset === current_fs) {
+        document.querySelectorAll("#progressbar li")[index].classList.remove("active");
+      }
+    });
+
+    // show the previous fieldset
+    previous_fs.style.display = "block";
+
+    // hide the current fieldset with style
+    var animationInterval = setInterval(function() {
+      current_fs.style.opacity -= 0.01;
+
+      // as the opacity of current_fs reduces to 0 - stored in "now"
+      // 1. scale previous_fs from 80% to 100%
+      scale = 0.8 + (1 - current_fs.style.opacity) * 0.2;
+      // 2. take current_fs to the right(50%) - from 0%
+      left = ((1 - current_fs.style.opacity) * 50) + "%";
+      // 3. increase opacity of previous_fs to 1 as it moves in
+      opacity = 1 - current_fs.style.opacity;
+      current_fs.style.left = left;
+      previous_fs.style.transform = 'scale(' + scale + ')';
+      previous_fs.style.opacity = opacity;
+
+      if (current_fs.style.opacity <= 0) {
+        current_fs.style.display = 'none';
+        animating = false;
+        clearInterval(animationInterval);
+      }
+    }, 5);
+  });
+});
+
+document.querySelectorAll(".submit").forEach(function(element) {
+  element.addEventListener("click", function() {
+    return false;
+  });
+});
+/*=============== Search ===============*/
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close'),
+      loginContent = document.getElementById('login-content')
+
+/*===== Search SHOW =====*/
+/* Validate if constant exists */
+if(loginButton){
+    loginButton.addEventListener('click', () =>{
+        loginContent.classList.add('show-login')
+    })
+}
+
+/*===== Search HIDDEN =====*/
+/* Validate if constant exists */
+if(loginClose){
+    loginClose.addEventListener('click', () =>{
+        loginContent.classList.remove('show-login')
+    })
+}
+
+/*=============== ADD SHADOW HEADER ===============*/
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () =>{
     const header = document.getElementById('header')
@@ -72,159 +150,210 @@ const scrollHeader = () =>{
 }
 window.addEventListener('scroll', scrollHeader)
 
+/*=============== DARK LIGHT THEME ===============*/
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
+// Previously selected theme (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
-/* choose faq */
-const faqItems = document.querySelectorAll('.choose__faq-item')
-// 1.select each item
-faqItems.forEach( (item) =>{
-    const faqHeader = item.querySelector('.choose__faq-header')
+// Obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
-    // select each button click
-    faqHeader.addEventListener('click' , () =>{
-        // select the current faq-open class
-        const openItem = document.querySelector('.faq-open')
-        // call the  toggleitem function
-        toggleItem(item)
-        // remove the faq-open class from other items
-        if(openItem && openItem != item){
-            toggleItem(openItem)
-        }
-    })
-})
-// creat function to display the contect
-const toggleItem = (item) =>{
-    // select each faq contecnt
-    const faqContent =item.querySelector('.choose__faq-content')
-    // if the same item contains the faq-open class ,remove
-    if(item.classList.contains('faq-open')){
-        faqContent.removeAttribute('style')
-        item.classList.remove('faq-open')
-
-    } else{
-            // add max-height to use the content and the faq-open class
-    faqContent.style.height = faqContent.scrollHeight + 'px'
-    item.classList.add('faq-open')
-    }
-
+// Validate if the user previously chose a theme
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark theme
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
 }
 
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // Save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
 
-
-// /*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
+/*=============== HOME SWIPER ===============*/
+let swiperProjects = new Swiper(".projects__container", {
   loop: true,
-  spaceBetween: 32,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+ },
+ pagination:{
+  el:".swiper-pagination",
+ },
+ breakpoints:{
+  1200:{
+    slidesPerView:2,
+    spaceBetween:-56,
+  },
+ },
+});
+
+/*=============== SWIPER POPULAR ===============*/
+
+let swiperFavorite = new Swiper('.favorite__swiper', {
+  loop: true,
+  slidesPerView:"auto",
+  centeredSlides:"auto",
+  grabCursor:true,
+  breakpoints:{
+    768:{
+      slidesPerView:4,
+    }
+  }
+});
+
+/*=============== Testimonial ===============*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  loop: true,
+  spaceBetween: 24,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    600: {
-      slidesPerView: 2,
-    },
-    968: {
-      slidesPerView: 3,
-    },
   },
   autoplay: {
-    delay: 6000,
-    disableOnInteraction: false,
+    delay: 4000, // تحديد مدة زمنية بالميللي ثانية، هنا تم تحديدها لتكون 4 ثواني
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
   },
 });
-// serv
-const model =document.querySelectorAll('.services__model'),
-  modelButton = document.querySelectorAll('.serv__botton'),
-  modelClose =  document.querySelectorAll('.services__model-close')
 
-  let activeModel = (modelClick) =>{
-    model[modelClick].classList.add('active-model')
-  }
-  modelButton.forEach((modelButton, i) =>{
-    modelButton.addEventListener('click', ()=>{
-      activeModel(i)
-    })
-  })
-  modelClose.forEach((modelClose) =>{
-    modelClose.addEventListener('click',() =>{
-      model.forEach((modelRemove) =>{
-        modelRemove.classList.remove('active-model')
-      })
-    })
-  })
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
-// 
-$(document).ready(function() {
-  $(".testimonial__img").click(function() {
-    // Toggle the "enlarged" class on the clicked image
-    $(this).toggleClass("enlarged");
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-    // Automatically revert to the original size after 5 seconds
-    setTimeout(function() {
-      $(".testimonial__img").removeClass("enlarged");
-    }, 5000);
-  });
-});
-
-/*==================== VIDEO ====================*/
-const videoFile = document.getElementById('video-file');
-const videoButton = document.getElementById('video-button');
-const videoIcon = document.getElementById('video-icon');
-var video = document.getElementById("video-file");
-video.volume = 0.5; 
-function playPause() {
-    if (videoFile.paused) {
-        // Play video
-        videoFile.play();
-        // Change the icon
-        videoIcon.classList.remove('fa-play');
-        videoIcon.classList.add('fa-pause');
-    } else {
-        // Pause video
-        videoFile.pause();
-        // Change the icon
-        videoIcon.classList.remove('fa-pause');
-        videoIcon.classList.add('fa-play');
-    }
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
 }
-
-videoButton.addEventListener('click', playPause);
-
-function finalVideo() {
-    // Video ends, icon change
-    videoIcon.classList.remove('fa-pause');
-    videoIcon.classList.add('fa-play');
-}
-
-// When the video ends
-videoFile.addEventListener('ended', finalVideo);
+window.addEventListener('scroll', scrollActive)
 
 
 
-// const sections = document.querySelectorAll('section[id]')
 
-// function scrollActive(){
-//   const scrollY = window.pageYOffset
 
-//   sections.forEach(current =>{
-//       const sectionHeight = current.offsetHeight
-//       const sectionTop = current.offsetTop - 50;
-//       sectionId = current.getAttribute('id')
 
-//       if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//           document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-//       }else{
-//           document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+// document.addEventListener("DOMContentLoaded", function () {
+//   const counterSection = document.getElementById("counter-section");
+//   const happyUsers = document.getElementById("happy-users");
+//   const followers = document.getElementById("followers");
+//   const experience = document.getElementById("experience");
+
+//   // Set your target numbers
+//   const targetHappyUsers = 100;
+//   const targetFollowers = 20;
+//   const targetExperience = 5;
+
+//   let counter = 0;
+
+//   function updateCounter() {
+//       if (counter < targetHappyUsers * 1000) {
+//           counter += 100;
+//           happyUsers.innerText = "+" + counter / 1000;
 //       }
-//   })
-// }
-// window.addEventListener('scroll', scrollActive)
+
+//       if (counter < targetFollowers * 1000) {
+//           counter += 50;
+//           followers.innerText = "+" + counter / 1000;
+//       }
+
+//       if (counter < targetExperience) {
+//           counter += 1; 
+//           experience.innerText = "+" + counter;
+//       } else if (counter === targetExperience) {
+//           experience.innerText = "+" + counter + "K";
+//       }
+
+//       if (counter < targetExperience || counter < targetFollowers * 1000 || counter < targetHappyUsers * 1000) {
+//           requestAnimationFrame(updateCounter);
+//       } else {
+//           counterSection.classList.add("show");
+//       }
+//   }
+
+//   updateCounter();
+// });
+/*=============== SWIPER HOME2 ===============*/
+const swipeHome2 = new Swiper('.home__swiper', {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+  effect: 'creative',
+  creativeEffect: {
+    prev: {
+      translate: [-100, 0, -500],
+      rotate: [0, 0, 15],
+      opacity: 0
+    },
+    next: {
+      translate: [100, 0, -500],
+      rotate: [0, 0, -15],
+      opacity: 0
+    },
+  },
+  // Autoplay configuration
+  autoplay: {
+    delay: 2000, // 5 seconds
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+/*=============== FAQ ===============*/
+const accordionItems = document.querySelectorAll('.questions__item')
+accordionItems.forEach((item)=>{
+    const accordionHeader = item.querySelector('.questions__header')
+    accordionHeader.addEventListener('click',()=>{
+        const openItem = document.querySelector('.accordion-open')
+        toggleItem(item)
+        if(openItem && openItem!==item){
+            toggleItem(openItem)
+        }
+    })
+})
+const toggleItem =(item)=>{
+    const accordionContent =item.querySelector('.questions__content')
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+ 
+}
 
 var loader =document.getElementById("preloader");
 window.addEventListener("load", function() {
