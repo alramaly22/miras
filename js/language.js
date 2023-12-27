95358ff63f4686511f394842a48495a75e18c187
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2023-12-27 02:36:02 +0200

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,292 @@
+let lang = {
+    ar: {
+        navhome: "الرئيسية",
+        navabout: "حول",
+        navcourse: "الدورة",
+        navpricing: "التسعير",
+        navtransformation: "التحول",
+        
+        homeh1p1: "افعليها من أجلك",
+        homeh1p2: "سأساعدك",
+        homeh1p3: "لتصبحي أقوى",
+        homebutton2: "ابدأ الآن",
+        listp: "ابدأ الآن لأن الخطوة الأولى هي الاشتراك، والخطوة الثانية هي اتباع النظام، وفي النهاية، ستحقق جسمًا رشيقًا وأنيقًا",
+
+        about2: "عن ميرا",
+        aboutp2: "كنت أعاني من اضطرابات في تناول الطعام، كنت أكافح مع صورتي الجسدية وكنت أكره جسمي ونفسي. في صغري، كنت أتعرض للتنمر، لذلك كانت ثقتي منخفضة للغاية. كنت أكره أي شخص يرغب في التقاط صور لي، ولم أكن أحب الكاميرات أبدًا! كنت خجولة جدًا وطائشة. عندما بدأت في الصالة الرياضية، لم أكن أفعل ذلك لأظهر بشكل معين، بل كنت أفعله لتحويل نفسي تمامًا، وأصبح أكثر ثقة، وأشعر بالقوة والقوة.",
+        
+
+        youtube: "قناتي على يوتيوب",
+        youtubebutton: "رؤية المزيد",
+ 
+        questionh2: "لماذا اختيار ميرا؟",
+        q1: "كيف يمكن لميرا مساعدتي في تحويل جسمي؟",
+        a1: "تستفيد ميرا من خبرتها لتوجيهك ودعمك في تحقيق تحول جسدي. تركز أساليبها المثبتة ليس فقط على التغييرات الجسدية ولكن أيضًا على تعزيز الرفاهية العامة.",
+        q2: "ما الذي يميز ميرا في مجال تدريب اللياقة؟",
+        a2: "يتجاوز نهج ميرا للتدريب الجسدي. إنها تفهم الجوانب العقلية والعاطفية للياقة البدنية، وتخلق بيئة داعمة للعملاء. تحول تدريبها لا يُحدد الجسم فقط، بل يمكن أيضًا للأفراد أن يقودوا حياةً أكثر إشباعًا.",
+        q3: "كيف تُعدل ميرا خطط اللياقة البدنية لتناسب احتياجات الأفراد؟",
+        a3: "تفهم ميرا أن كل فرد فريد. تُخصص خطط اللياقة البدنية لتلبية احتياجاتك الخاصة، مع مراعاة عوامل مثل مستوى اللياقة البدنية والحالة الصحية والأهداف الشخصية، مما يضمن نهجًا شخصيًا وفعّالًا.",
+        q4: "كيف تعالج ميرا الحواجز العقلية في رحلات اللياقة البدنية؟",
+        a4: "تتجاوز ميرا التدريب الجسدي بمعالجة الحواجز العقلية. يتضمن تدريبها استراتيجيات للتغلب على الشكوك الذاتية، وزرع الثقة، وخلق تفكير إيجابي لتحقيق تحول شامل ودائم.",
+        q5: "ما الدعم الذي تقدمه ميرا للتوجيه التغذوي؟",
+        a5: "تفهم ميرا أهمية التغذية في تحقيق أهداف اللياقة البدنية. تقدم إرشادات تغذية شاملة، تساعدك في اتخاذ قرارات مستنيرة لتغذية جسمك بشكل أمثل وتكمل رحلتك في اللياقة.",
+        q6: "كيف تخلق ميرا مجتمعًا داعمًا للياقة البدنية؟",
+        a6: "تعزز ميرا مجتمعًا داعمًا للياقة البدنية من خلال تشجيع التفاعلات المنتظمة بين العملاء. تيسر الأنشطة الجماعية، وتوفر منصة لتحقيق الإنجازات المشتركة، وتعزز بيئة إيجابية حيث يتحفز ويلهم الأفراد بعضهم البعض طوال رحلاتهم في اللياقة.",
+        q7: "كيف تجعل ميرا برامجها الرياضية جذابة وممتعة؟",
+        a7: "تصمم ميرا برامج رياضية ديناميكية وممتعة بمزج مجموعة متنوعة من التمارين والروتينات والتحديات. تعتقد أن الحفاظ على المتعة والتنوع في التمارين لا يُحسن فقط النتائج الجسدية، بل يضمن أيضًا أن يظل العملاء متحفزين ومتحمسين بشأن رحلتهم في اللياقة.",
+        q8: "ما هو دور التفكير في فلسفة تدريب ميرا؟",
+        a8: "تولي ميرا أهمية كبيرة للتفكير في فلسفتها التدريبية. تعتقد أن التفكير الإيجابي والقوي أمر أساسي للتغلب على التحديات، والبقاء ثابتًا، وتحقيق النجاح على المدى الطويل في اللياقة. يشمل تدريبها استراتيجيات لزرع تفكير يدعم النمو الشخصي والرفاهية.",
+        q9: "كيف تكيف ميرا برامجها لتناسب مستويات اللياقة المختلفة؟",
+        a9: "تُخصص ميرا برامج اللياقة البدنية لتكييفها مع مستويات اللياقة المختلفة. سواء كنت مبتدئًا أو رياضيًا متمرسًا، تُخصص ميرا التمارين ومستويات الشدة لضمان أن يمكن لكل فرد التقدم بوتيرة خاصة به، مما يعزز مجتمعًا شاملاً وداعمًا للياقة.",
+        q10: "ما الدعم المستمر الذي تقدمه ميرا لعملائها بعد اكتمال البرنامج؟",
+        a10: "تمتد التزامات ميرا بعد اكتمال البرنامج. تقدم دعمًا مستمرًا من خلال موارد ما بعد البرنامج، واستشارات متابعة، ومنصة مجتمع حيث يمكن للعملاء الاستمرار في التواصل وتبادل التجارب والحصول على إرشاد، مما يضمن تأثيرًا مستمرًا وإيجابيًا على لياقتهم ورفاهيتهم.",
+
+        testimonialh2: "شهادات العملاء",
+        testimonialname1: "سارة جونسون",
+        testimonialname2: "أليكس رودريجيز",
+        testimonialname3: "إميلي تومبسون",
+        testimonialp1: "الانضمام كانت خطوة حاسمة! تحقيق أهداف اللياقة البدنية مع التوجيه الشخصي والدعم الملحوظ من المدربين. جعلت المجتمع المشجع تقاسم رحلتي حقًا مجزية",
+        testimonialp2: "تجربة تحولية مع مدربين رائعين. قصتك تهم هنا. انضم، شارك، ولنمكِّن بعضنا البعض",
+        testimonialp3: "واحدة من أفضل القرارات. دعم وإلهام لا مثيل له. لا تفوت الفرصة - انضم إلينا وشارك إنجازاتك",
+
+        homep1: "افعلي هذا",
+        homep3: "من أجل",
+        homep2: "نفسك ",
+        homep: "كيف تصبحين امرأة أقوى من الداخل، في رحلة تحولية داخل مجتمعنا المميز",
+        homebutton: "ابدأ الآن",
+        homespan1: "مستخدمين سعداء",
+        homespan2: "متابعين",
+        homespan3: "تجربة",
+
+        abouth2: "حول ميرا سبور",
+        aboutp: "اسمي ميرا سبور. هل تريد الحصول على جسم رائع؟ انضم إلي. اسمي ميرا صوفيا، وُلدت في 29 مايو 1997 في المدينة الجميلة ميامي، فلوريدا. أنا مؤثرة في مجال اللياقة البدنية وكانت بدايتي مختلفة عن غيري. كنت دائما أعاني من النحافة في سنوات المراهقة. لجأت إلى ممارسة التمارين الرياضية لكسر هذه الشكوك التي كنت أعاني منها طوال فترة المراهقة. كانت تؤمن أنها قادرة، لذلك فعلت!",
+        aboutbutton: "اعرف المزيد",
+
+
+
+        pricingh1: "أسعارنا",
+        pricingh2: "تحدي الأرداف",
+        pricingh3: "$99",
+        pricingh4: "الخصر الصغير والبطن المشدود",
+        pricingh5: "$99",
+        pricingh6: "فقدان الوزن",
+        pricingh7: "$99",
+        pricingbutton: "ابدأ الآن",
+        pricinga1:"٨ تمارين في الاسبوع",
+        pricinga2:"مقاطع فيديو تعليمية",
+        pricinga3:"التوجيه و العقلية في صاله الجيم",
+        pricinga4:"التقديم التدريجي",
+        pricinga5:"سؤال و جواب حول الدليل",
+        pricinga6:"مخطط النظام الغذائي و الوصفات",
+        pricinga7:"خطه التدريب كل اسبوع",
+        ppricinga8:"دعم البريد الإلكتروني",
+        pricinga9:"كيفية تتبع السعرات الحرارية و ضبطه حسب الأهداف",
+
+        coursesh2: "دوراتنا",
+        coursesh21: "تحدي الأرداف",
+        coursesp1: "نحت، شد، قوة! انضم للحصول على أرداف أكثر إحكامًا وشدًا. تمارين مخصصة، دعم المجتمع لتعزيز ثقتك بنفسك",
+        coursesh22: "الخصر الصغير والبطن المشدود",
+        coursesp2: "خصر صغير، بطن مشدود: برنامج متخصص، تمارين مخصصة، إرشادات خبيرة. اكتشف أسرار الثقة بالنفس والتمثيل لنفسك المشدودة",
+        coursesh23: "فقدان الوزن",
+        coursesp3: "تمارين مخصصة، إرشادات تغذية، ودعم المجتمع لفقدان الوزن المستدام. حقق أهداف اللياقة الخاصة بك اليوم",
+        
+        transformation: "تحولات عملاؤنا",
+
+        contactcontactme: "تواصل معي",
+        conatctwrite: "اكتب لي أسئلتك",
+        contactusername: "الأسماء",
+        contactemail: "البريد الإلكتروني",
+        contactmessage: "الرسالة",
+        contactbutton: "إرسال الرسالة",
+
+        footerp: "افعلها من أجلك سأساعدك لتكون أقوى",
+        footerlinks: "روابط",
+        footera6: "الرئيسية",
+        footera5: "الدورات",
+        footera7: "التسعير",
+        footera8: "التحول",
+        footerinformation: "المعلومات",
+        footera1: "حول",
+        footera2: "الأسئلة الشائعة",
+        footera3: "الشهادات",
+        footera4: "تواصل معنا",
+        footercontact: "تواصل معنا",
+    },
+    en: {
+        navhome:"Home",
+        navabout:"About",
+        navcourse:"Course",
+        navpricing:"Pricing",
+        navtransformation:"Transformation",
+
+        homeh1p1:"Do It For You",
+        homeh1p2:"I Will Help You",
+        homeh1p3:"To Be Stronger",
+        homebutton2:"Get Start",
+
+        about2:"About MiraSpor",
+        aboutp2:"I used to suffer from eating disorders, I used to struggle with my body image and hate my body and myself. When I was younger I used to be bullied so my confidence used to be very low. I used to hate anyone who wanted to take pictures of myself and I never liked cameras! I used to be very timid and nerdy. When I started the gym, I wasn’t doing it to look in a certain way, I was doing it to completely transform myself, become more confident, feel stronger and powerful",
+       
+        listp:"Get start now because the first step is to subscribe, the second step is to follow the system, and in the end, you'll achieve a sleek and elegant body",
+
+        youtube:"My Youtube Channel",
+        youtubebutton:"See More",
+
+        questionh2:"Why Choose MiraSpor?",
+        q1:"How can Mira Sporr help me transform my body?",
+        a1:"Mira Sporr leverages her expertise to guide and support you in achieving a transformative body. Her proven methods focus not only on physical changes but also on enhancing overall well-being",
+        q2:"What sets MiraSpor apart in fitness coaching?",
+        a2:"Mira Sporr's approach goes beyond physical training. She understands the mental and emotional aspects of fitness, creating a supportive environment for clients. Her coaching not only transforms bodies but also empowers individuals to lead more fulfilling lives",
+        q3:"How does MiraSpor tailor fitness plans to individual needs?",
+        a3:"Mira Sporr understands that every individual is unique. She customizes fitness plans to your specific needs, considering factors such as fitness level, health conditions, and personal goals, ensuring a personalized and effective approach",
+        q4:"How does MiraSpor address mental barriers in fitness journeys?",
+        a4:"Mira Sporr goes beyond physical training by addressing mental barriers. Her coaching incorporates strategies to overcome self-doubt, instill confidence, and create a positive mindset for a holistic and lasting transformation",
+        q5:"What support does MiraSpor provide for nutritional guidance?",
+        a5:"Mira Sporr understands the importance of nutrition in achieving fitness goals. She offers comprehensive nutritional guidance, helping you make informed choices to fuel your body optimally and complement your fitness journey",
+        q6:"How does MiraSpor create a supportive fitness community?",
+        a6:"Mira Sporr fosters a supportive fitness community by encouraging regular interactions among clients. She facilitates group activities, provides a platform for shared achievements, and promotes a positive environment where individuals motivate and inspire each other throughout their fitness journeys",
+        q7:"How does MiraSpor keep her fitness programs engaging and enjoyable?",
+        a7:"Mira Sporr designs dynamic and enjoyable fitness programs by incorporating a variety of exercises, routines, and challenges. She believes that keeping workouts fun and diverse not only enhances physical results but also ensures that clients stay motivated and excited about their fitness journey",
+        q8:"What role does mindset play in MiraSpor's coaching philosophy?",
+        a8:"Mira Sporr places a significant emphasis on mindset in her coaching philosophy. She believes that a positive and resilient mindset is crucial for overcoming challenges, staying consistent, and achieving long-term success in fitness. Her coaching includes strategies to cultivate a mindset that supports personal growth and well-being",
+        q9:"How does MiraSpor adapt her programs to cater to different fitness levels?",
+        a9:"Mira Sporr customizes her fitness programs to accommodate various fitness levels. Whether you're a beginner or an experienced athlete, Mira tailors exercises and intensity levels to ensure that each individual can progress at their own pace, fostering an inclusive and supportive fitness community",
+        q10:"What ongoing support does MiraSpor provide to her clients after program completion?",
+        a10:"Mira Sporr's commitment extends beyond program completion. She offers ongoing support through post-program resources, follow-up consultations, and a community platform where clients can continue to connect, share experiences, and receive guidance, ensuring a sustained and positive impact on their fitness and well-being",
+
+        testimonialh2:"Testimonials",
+        testimonialname1:"Sarah Johnson",
+        testimonialname2:"Alex Rodriguez",
+        testimonialname3:"Emily Thompson",
+        testimonialp1:"Joining was a game-changer! Achieved fitness goals with personalized guidance and remarkable trainer support. The encouraging community made sharing my journey truly rewarding",
+        testimonialp2:"Transformative experience with great trainers. Your story matters here. Join, share, and let's empower each other",
+        testimonialp3:"One of the best decisions. Unparalleled support and inspiration. Don't miss out—join us and share your achievements",
+        
+
+        homep1:"DO THIS",
+        homep2:"FOR",
+        homep3:"YOU",
+        homep:"How To Be A Stronger Woman From The Inside Out , for a transformative journey in our empowering community",
+        homebutton:"Get Start",
+        homespan1:"Happy User",
+        homespan2:"Followers",
+        homespan3:"Experience",
+
+        abouth2:"About MiraSpor",
+        aboutp:"My Name Is Mira Sporr Do You Want To Have A Great Body Join Me My Name Is Mira Sofia , Born On May 29th, 1997 In The Beautiful Miami, Florida. I Am A Fitness Influencer Who’s Had A Different Start To My Journey Compared To Others. I Had Always Struggled With Being Under-Weight In My Teenage Years. I Sought Out To Fitness To Break Me Out Of This Insecurity That I Struggled With, All Throughout My Adolescence. She Believed She Could. So She Did!",
+        aboutbutton:"Know more",
+        
+        coursesh2:"Our Courses",
+        coursesh21:"Booty Challenge ",
+        coursesp1:"Sculpt, tone, empower! Join for firmer, toned booty. Tailored workouts, community support for a confident you",
+        coursesh22:"Abs Tiny Waist",
+        coursesp2:"Tiny waist, sculpted abs: Specialized program, tailored workouts, expert guidance. Unlock secrets to a confident, sculpted you",
+        coursesh23:"Weight Loss",
+        coursesp3:"Tailored workouts, nutrition guidance, and community support for sustainable weight loss. Achieve your fitness goals today",
+
+
+
+
+        transformation:"Transformation",
+
+        pricingh1:"Our Pricing",
+        pricingh2:"Booty Challenge",
+        pricingh3:"$99",
+        pricingh4:"Abs Tiny Waist",
+        pricingh5:"$99",
+        pricingh6:"Weight Loss",
+        pricngh7:"$99",
+        pricingbutton:"Get Start",
+        pricinga1:"8 WorKouts Per Week",
+        pricinga2:"Instructional Videos",
+        pricinga3:"Guidance & Mindset In The Gym",
+        pricinga4:"Progressive Overload",
+        pricinga5:"Q&A About The Guide",
+        pricinga6:"Dietplan & Recipes",
+        pricinga7:"Trainingplan For Every Week",
+        ppricinga8:"Support Trough E-mail",
+        pricinga9:"How To Track Calories And Adjust It To Your Goals",
+        
+        contactcontactme:"Contact Me",
+        conatctwrite:"Write me your Questions",
+        contactusername:"Names",
+        contactemail:"Mail",
+        contactmessage:"Message",
+        contactbutton:"Send Message",
+        
+        footerp: "Do It For You I Will Help You To Be Stronger",
+        footerlinks:"Links",
+        footera6:"Home",
+        footera5:"Courses",
+        footera7:"Pricing",
+        footera8:"Transformation",
+        footerinformation:"Information",
+        footera1:"About",
+        footera2:"Faqs",
+        footera3:"Testimonials",
+        footera4:"Contact",
+        footercontact:"Contact Us",
+        }
+};
+
+let selector = document.getElementById('language-selector');
+
+// استدعاء الدالة عند تغيير اللغة
+selector.addEventListener('change', updateLanguage);
+
+function updateLanguage() {
+    let language = selector.value; // اللغة المختارة
+    let nodes = document.querySelectorAll('[data-lang]');
+
+    // تحديث نصوص العناصر باستخدام اللغة المناسبة
+    nodes.forEach(node => {
+        let key = node.getAttribute('data-lang');
+        if (lang[language] && lang[language][key]) {
+            node.textContent = lang[language][key];
+        }
+    });
+}
+
+
+// استدعاء الدالة لتعيين اللغة الافتراضية عند تحميل الصفحة
+updateLanguage();
+
+function updateLanguage() {
+    let language = selector.value; // اللغة المختارة
+    let nodes = document.querySelectorAll('[data-lang]');
+
+    // تحديث نصوص العناصر وتبديل الفئات الخاصة بـ font-family
+    nodes.forEach(node => {
+        let key = node.getAttribute('data-lang');
+        if (lang[language] && lang[language][key]) {
+            node.textContent = lang[language][key];
+        }
+
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('arabic-font');
+        } else {
+            node.classList.remove('arabic-font');
+            node.classList.add('english-font');
+        }
+
+        // إضافة الفئة الإضافية هنا (مثل 'second-arabic-font') إذا كان ذلك مطلوبًا
+        // وتبديلها بناءً على اللغة المحددة بنفس الطريقة
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('second-arabic-font');
+        } else {
+            node.classList.remove('second-arabic-font');
+            node.classList.add('english-font');
+        }
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('se-arab');
+        } else {
+            node.classList.remove('se-arab');
+            node.classList.add('english-font');
+        }
+    });
+}
+
+
